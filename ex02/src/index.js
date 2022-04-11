bfa2a540c572594fba23b396b04fb1c49ff277c0
belmapeskic <belma.peskic@gmail.com>
2022-04-12 01:07:28 +0200

    Adding file index.js

---
+++ ex02/src/index.js
@@ -0,0 +1,16 @@
+function checkVariablescope() {
+    "use strict";
+    // only change code below this line
+    let i = 'function variable';
+    // only change code above this line
+    if (true) {
+        // only change code below this line
+        let i = 'block variable';
+        // only change code above this line
+        console.log('Scope i is: ', i);
+    }
+    console.log('Scope i is: ', i);
+    return i;
+}
+checkVariablescope();
+module.exports = checkVariablescope;
