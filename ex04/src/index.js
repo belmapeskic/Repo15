b257a6849c785a916361c3443d6fc33ad37f535e
belmapeskic <belma.peskic@gmail.com>
2022-04-12 11:00:40 +0200

    Adding file index.js

---
+++ ex04/src/index.js
@@ -0,0 +1,10 @@
+function myArr(mili) {
+    "use strict";
+    // Only change code below this line
+    const milili = [10, 25, 4]; 
+    milili = [4, 10, 25]; // Using mili = [4, 10, 25] would be invalid
+    console.log(milili);
+    return milili;
+    // only change code above this line
+}
+module.exports = myArr;
