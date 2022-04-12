5232a3e57683f563af38e888c457b4e7a456a2e3
belmapeskic <belma.peskic@gmail.com>
2022-04-12 03:09:21 +0200

    Adding file index.js

---
+++ ex06/src/index.js
@@ -0,0 +1,8 @@
+// Only change code below this line
+const myArray = (array1, array2) => {
+    // Only change code above this line
+    "use strict";
+    return array1.concat(array2);
+};
+console.log(myArray([10, 11], [12, 13, 14, 15])); 
+module.exports = myArray;
